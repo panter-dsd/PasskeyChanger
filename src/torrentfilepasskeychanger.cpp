261031f6609a3d6af63f641c5cd8e5280dc388fb
PanteR <panter.dsd@gmail.com>
2011-06-29 23:44:21 +0400

    Begin adding TorrentFilePasskeyChanger.

---
+++ src/torrentfilepasskeychanger.cpp
@@ -0,0 +1,87 @@
+/*
+    <one line to give the program's name and a brief idea of what it does.>
+    Copyright (C) 2011  panter.dsd <panter.dsd@gmail.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <QtGui/QWidget>
+#include <QtGui/QLabel>
+#include <QtGui/QLineEdit>
+#include <QtGui/QToolButton>
+#include <QtGui/QHBoxLayout>
+#include <QtGui/QFileDialog>
+
+#include "torrentfilepasskeychanger.h"
+
+TorrentFilePasskeyChanger::TorrentFilePasskeyChanger (QObject *parent)
+	: AbstractPasskeyChanger (parent)
+{
+	settingsWidget_ = new QWidget ();
+
+	pathLabel_ = new QLabel (tr ("Path to torrent file"), settingsWidget_);
+
+	pathEdit_ = new QLineEdit (settingsWidget_);
+
+	pathButton_ = new QToolButton (settingsWidget_);
+	pathButton_->setText ("...");
+	connect (pathButton_, SIGNAL (clicked(bool)), SLOT (getFilePath()));
+
+	QHBoxLayout *layout = new QHBoxLayout ();
+	layout->addWidget (pathLabel_);
+	layout->addWidget (pathEdit_);
+	layout->addWidget (pathButton_);
+	settingsWidget_->setLayout (layout);
+}
+
+TorrentFilePasskeyChanger::~TorrentFilePasskeyChanger()
+{
+
+}
+
+const QWidget *TorrentFilePasskeyChanger::settingsWidget_p () const
+{
+
+}
+
+QByteArray TorrentFilePasskeyChanger::saveState_p () const
+{
+
+}
+
+bool TorrentFilePasskeyChanger::restoreState_p (const QByteArray &state)
+{
+
+}
+
+bool TorrentFilePasskeyChanger::isReady_p () const
+{
+
+}
+
+bool TorrentFilePasskeyChanger::changePasskey_p (const QString &oldPasskey, const QString &newPasskey)
+{
+
+}
+
+void TorrentFilePasskeyChanger::getFilePath ()
+{
+	const QString fileName = QFileDialog::getOpenFileName (settingsWidget_,
+														   tr ("Open torrent file"),
+														   QDir (pathEdit_->text()).absolutePath (),
+														   tr ("Torrent files (*.torrent)"));
+	if (!fileName.isEmpty ()) {
+		pathEdit_->setText (fileName);
+	}
+}
---
+++ src/torrentfilepasskeychanger.h
@@ -0,0 +1,60 @@
+/*
+    <one line to give the program's name and a brief idea of what it does.>
+    Copyright (C) 2011  panter.dsd <panter.dsd@gmail.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef TORRENTFILEPASSKEYCHANGER_H
+#define TORRENTFILEPASSKEYCHANGER_H
+
+#include "abstractpasskeychanger.h"
+
+class QLabel;
+class QLineEdit;
+class QToolButton;
+
+class TorrentFilePasskeyChanger : public AbstractPasskeyChanger
+{
+	Q_OBJECT
+
+public:
+	TorrentFilePasskeyChanger (QObject *parent = 0);
+	virtual ~TorrentFilePasskeyChanger();
+
+private:
+	TorrentFilePasskeyChanger (const TorrentFilePasskeyChanger &other);
+	TorrentFilePasskeyChanger &operator= (const TorrentFilePasskeyChanger &other);
+
+private:
+	virtual const QWidget *settingsWidget_p () const;
+	virtual QByteArray saveState_p () const;
+	virtual bool restoreState_p (const QByteArray &state);
+	virtual bool isReady_p () const;
+	virtual bool changePasskey_p (const QString &oldPasskey, const QString &newPasskey);
+	virtual QString name_p () const {
+		return tr ("Torrent file changer");
+	}
+
+private Q_SLOTS:
+	void getFilePath ();
+
+private:
+	QWidget *settingsWidget_;
+	QLabel *pathLabel_;
+	QLineEdit *pathEdit_;
+	QToolButton *pathButton_;
+};
+
+#endif // TORRENTFILEPASSKEYCHANGER_H
